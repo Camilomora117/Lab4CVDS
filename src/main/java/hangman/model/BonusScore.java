61cc7eed8084b745de98fcf328b6342bf1586841
larfg <luis.giraldo-r@mail.escuelaing.edu.co>
2022-02-15 18:43:36 -0500

    Especificación metodos

---
+++ src/main/java/hangman/model/BonusScore.java
@@ -0,0 +1,16 @@
+package hangman.model;
+
+public class BonusScore implements GameScore{
+
+    /**
+     * @pre Entra el numero de aciertos y el numero de letras incorrectas
+     * @pos Devuelve el puntaje final teniendo en cuenta las condiciones dadas
+     * 
+     * @param correctCount int Numero de letras correctas
+     * @param incorrectCount int Numero de letras incorrectas
+     * 
+     */
+    public int calculateScore(int correctCount,int incorrectCount){
+        return 0;
+    }
+}
---
+++ src/main/java/hangman/model/PowerScore.java
@@ -0,0 +1,17 @@
+package hangman.model;
+
+
+public class PowerScore implements GameScore{
+    /**
+     * @pre Entra el numero de aciertos y el numero de letras incorrectas
+     * @pos Devuelve el puntaje final teniendo en cuenta las condiciones
+     * 
+     * @param correctCount int Numero de letras correctas
+     * @param incorrectCount int Numero de letras incorrectas
+     * 
+     */
+    public int calculateScore(int correctCount,int incorrectCount){
+        return 0;
+    }
+
+}
